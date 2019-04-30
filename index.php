17d2df5edc0c73d0e66b86f69df0e7001e669640
waghtot <waghtot@gmail.com>
2019-04-30 22:58:13 +0100

    block site

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
 <?php
-session_start();
-require_once "app/views/templates/secure.php";
+// session_start();
+// require_once "app/views/templates/secure.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    </head>
+    <body>
+        <p>Find me in uk</p>
+    </body>
+</html>
